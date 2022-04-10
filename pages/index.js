f5ebf7430e1d11c2eacfb1da51f5a58b08ff7be4
ahnjh <algorithm123@naver.com>
2022-04-10 20:17:16 +0900

    fix: css 화면 가운데정렬
    leftBody, rightBody table-cell + vertical-align: middle

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import Image from "next/image";
 import { AiFillGithub, AiOutlineMail, AiFillPhone } from "react-icons/ai";
 import { RiKakaoTalkFill } from "react-icons/ri";
 
-const leftColor = "#707070";
+const leftColor = "#7F7F7F";
 const rightColor = "#FFF";
 
 const MainSubject = styled.div`
@@ -67,45 +67,51 @@ const Root = styled.div`
             
             width: 50%;
             height: 100%;
-            
-            padding-top: 100px;
-            
             text-align: center;
+            margin: auto;
             
-            .imageSlide {
-                //overflow: hidden;
-                //font-size: 0;
-                //white-space: nowrap;
-                //width: 100%;
-                //height: 100%;
-                //transition: text-indent 500ms;
-                //position: relative;
-                display: flex;
-                justify-content: center;
+            display: table;
+            
+            .leftContent {
+                display: table-cell;
+                backgorund-color: red;
+                vertical-align: middle;
                 
-                .image1 {
-                    width: 600px;
-                    height: 450px;
-                    background-image: url("/images/A.png");
-                    background-size: cover;
-                    box-shadow: 1px 1px 2px grey, 0 0 25px white, 0 0 5px lightgrey;
+                .imageSlide {
+                    //overflow: hidden;
+                    //font-size: 0;
+                    //white-space: nowrap;
+                    //width: 100%;
+                    //height: 100%;
+                    //transition: text-indent 500ms;
+                    //position: relative;
+                    display: flex;
+                    justify-content: center;
+                    
+                    .image1 {
+                        width: 600px;
+                        height: 450px;
+                        background-image: url("/images/A.png");
+                        background-size: cover;
+                        box-shadow: 1px 1px 2px grey, 0 0 25px white, 0 0 5px lightgrey;
+                    }
+                    
+                    //.image2 {
+                    //    width: 600px;
+                    //    height: 450px;
+                    //    background-image: url("/images/B.png");
+                    //    background-size: cover;
+                    //    box-shadow: 1px 1px 2px grey, 0 0 25px white, 0 0 5px lightgrey;                    
+                    //}
+                    //
+                    //.image3 {
+                    //    width: 600px;
+                    //    height: 450px;
+                    //    background-image: url("/images/C.png");
+                    //    background-size: cover;
+                    //    box-shadow: 1px 1px 2px grey, 0 0 25px white, 0 0 5px lightgrey;                    
+                    //}
                 }
-                
-                //.image2 {
-                //    width: 600px;
-                //    height: 450px;
-                //    background-image: url("/images/B.png");
-                //    background-size: cover;
-                //    box-shadow: 1px 1px 2px grey, 0 0 25px white, 0 0 5px lightgrey;                    
-                //}
-                //
-                //.image3 {
-                //    width: 600px;
-                //    height: 450px;
-                //    background-image: url("/images/C.png");
-                //    background-size: cover;
-                //    box-shadow: 1px 1px 2px grey, 0 0 25px white, 0 0 5px lightgrey;                    
-                //}
             }
         }
         .leftBody::after {
@@ -119,45 +125,72 @@ const Root = styled.div`
             
             width: 50%;
             height: 100%;
-            padding-top: 100px;
             
             text-align: center;
             
-            .introTitle {
-                font-size: 30px;
-                
-                margin-bottom: 33px;
-            }
+            display: table;
             
-            .introText {
-                font-size: 18px;
+            .rightContent {
+                display: table-cell;
+                vertical-align: middle;
                 
-                font-weight: lighter;
-                
-                margin-bottom: 50px;
-            }
+                .introTitle {
+                    font-size: 30px;
+                    
+                    margin-bottom: 33px;
+                }
             
-            .socialMediaIcon {
-                margin-top: 30px;
-                
-            }
+                .introText {
+                    font-size: 18px;
+                    
+                    font-weight: lighter;
+                    
+                    margin-bottom: 50px;
+                }
             
-            button {
-                width: 350px;
-                height: 50px;
-                border: none;
+                .socialMediaIcon {
+                    margin-top: 30px;
+                    
+                    .aiFillGithub, 
+                    .aiOutlineMail, 
+                    .riKakaoTalkFill, 
+                    .aiFillPhone {
+                        display: inline;
+                        margin-left: 25px;
+                        margin-right: 25px;
+                        
+                        .aiFillGithubText, 
+                        .aiOutlineMailText, 
+                        .riKakaoTalkFillText, 
+                        .aiFillPhoneText {
+                            display: none;
+                        }
+                    }
+                    .aiFillGithub:hover, 
+                    .aiOutlineMail:hover, 
+                    .riKakaoTalkFill:hover, 
+                    .aiFillPhone:hover {
+                        cursor: pointer;
+                    }
+                }
                 
-                margin-bottom: 5px;
-                
-                transition:all 0.9s;
-            }
-            button:active {
-                outline: none;
-                box-shadow: none;
-            }
-            button:hover {
-                box-shadow: 0 80px 0 0 rgba(0,86,102,0.5) inset, 0 -80px 0 0 rgba(0,86,102,0.5) inset;
-                color: white;
+                button {
+                    width: 350px;
+                    height: 50px;
+                    border: none;
+                    
+                    margin-bottom: 5px;
+                    
+                    transition:all 0.9s;
+                }
+                button:active {
+                    outline: none;
+                    box-shadow: none;
+                }
+                button:hover {
+                    box-shadow: 0 80px 0 0 rgba(0,86,102,0.5) inset, 0 -80px 0 0 rgba(0,86,102,0.5) inset;
+                    color: white;
+                }
             }
         }
         .rightBody::after {
@@ -179,7 +212,6 @@ const Root = styled.div`
             
             width: 50%;
             height: 100%;
-            
         }
         
         .rightFooter {
@@ -216,84 +248,112 @@ export default function Home() {
 
             <div className="body">
                 <div className="leftBody">
-                    <div className="imageSlide">
-                        <div className="image1" onClick={e => {
-                            e.preventDefault();
+                    <div className="leftContent">
+                        <div className="imageSlide">
+                            <div className="image1" onClick={e => {
+                                e.preventDefault();
 
-                            alert("이미지 슬라이드 추가 예정");
-                        }}/>
-                        <div className="image2"/>
-                        <div className="image3"/>
-                        {/*<Image*/}
-                        {/*    src="/images/A.png"*/}
-                        {/*    width="600"*/}
-                        {/*    height="450"*/}
-                        {/*    alt="logo"*/}
-                        {/*    className="image1"*/}
-                        {/*/>*/}
-                        {/*<Image*/}
-                        {/*    src="/images/B.png"*/}
-                        {/*    width="600"*/}
-                        {/*    height="450"*/}
-                        {/*    alt="logo"*/}
-                        {/*    className="image2"*/}
-                        {/*/>*/}
-                        {/*<Image*/}
-                        {/*    src="/images/C.png"*/}
-                        {/*    width="600"*/}
-                        {/*    height="450"*/}
-                        {/*    alt="logo"*/}
-                        {/*    className="image3"*/}
-                        {/*/>*/}
+                                alert("이미지 슬라이드 추가 예정");
+                            }}/>
+                            <div className="image2"/>
+                            <div className="image3"/>
+                            {/*<Image*/}
+                            {/*    src="/images/A.png"*/}
+                            {/*    width="600"*/}
+                            {/*    height="450"*/}
+                            {/*    alt="logo"*/}
+                            {/*    className="image1"*/}
+                            {/*/>*/}
+                            {/*<Image*/}
+                            {/*    src="/images/B.png"*/}
+                            {/*    width="600"*/}
+                            {/*    height="450"*/}
+                            {/*    alt="logo"*/}
+                            {/*    className="image2"*/}
+                            {/*/>*/}
+                            {/*<Image*/}
+                            {/*    src="/images/C.png"*/}
+                            {/*    width="600"*/}
+                            {/*    height="450"*/}
+                            {/*    alt="logo"*/}
+                            {/*    className="image3"*/}
+                            {/*/>*/}
+                        </div>
                     </div>
                 </div>
 
                 <div className="rightBody">
-                    <div className="introTitle">
-                        개발자 안주환입니다.
-                    </div>
+                    <div className="rightContent">
+                        <div className="introTitle">
+                            개발자 안주환입니다.
+                        </div>
 
-                    <div className="introText">
-                        웹 풀사이클을 향해서 매일을 코드와 함께하고 있고, <br/>
-                        좋은 코드작성과 구조를 목표로 하고 있습니다. <br/>
-                    </div>
-                    <button onClick={e => {
-                        e.preventDefault();
+                        <div className="introText">
+                            웹 풀사이클을 향해서 매일을 코드와 함께하고 있고, <br/>
+                            좋은 코드작성과 구조를 목표로 하고 있습니다. <br/>
+                        </div>
+                        <button onClick={e => {
+                            e.preventDefault();
 
-                        router.push("/info");
-                    }}>
-                        INFO
-                    </button><br/>
+                            router.push("/info");
+                        }}>
+                            INFO
+                        </button><br/>
 
-                    <button onClick={e => {
-                        e.preventDefault();
+                        <button onClick={e => {
+                            e.preventDefault();
 
-                        alert("PROJECT");
-                    }}>
-                        PROJECT
-                    </button><br/>
+                            alert("PROJECT");
+                        }}>
+                            PROJECT
+                        </button><br/>
 
-                    <button onClick={e => {
-                        e.preventDefault();
+                        <button onClick={e => {
+                            e.preventDefault();
 
-                        alert("KNOWLEDGE");
-                    }}>
-                        KNOWLEDGE
-                    </button><br/>
+                            alert("KNOWLEDGE");
+                        }}>
+                            KNOWLEDGE
+                        </button><br/>
 
-                    <button onClick={e => {
-                        e.preventDefault();
+                        <button onClick={e => {
+                            e.preventDefault();
 
-                        alert("MENU4");
-                    }}>
-                        MENU4
-                    </button><br/>
+                            alert("MENU4");
+                        }}>
+                            MENU4
+                        </button><br/>
 
-                    <div className="socialMediaIcon">
-                        <AiFillGithub size="35" style={{marginRight: "50px"}}/>
-                        <AiOutlineMail size="35" style={{marginRight: "50px"}}/>
-                        <RiKakaoTalkFill size="35" style={{marginRight: "50px"}}/>
-                        <AiFillPhone size="35"/>
+                        <div className="socialMediaIcon">
+                        <span className="aiFillGithub">
+                            <AiFillGithub size="35"/>
+
+                            <span className="aiFillGithubText">
+
+                            </span>
+                        </span>
+                            <span className="aiOutlineMail">
+                            <AiOutlineMail size="35"/>
+
+                            <span className="aiOutlineMailText">
+                                algorithm123@naver.com
+                            </span>
+                        </span>
+                            <span className="riKakaoTalkFill">
+                            <RiKakaoTalkFill size="35"/>
+
+                            <span className="riKakaoTalkFillText">
+                                KT
+                            </span>
+                        </span>
+                            <span className="aiFillPhone">
+                            <AiFillPhone size="35"/>
+
+                            <span className="aiFillPhoneText">
+
+                            </span>
+                        </span>
+                        </div>
                     </div>
                 </div>
             </div>
