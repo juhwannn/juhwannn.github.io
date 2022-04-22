dc50d19aa6ae2777f5c7b4fbd012b8c027c31ebc
ahnjh <algorithm123@naver.com>
2022-04-22 16:58:52 +0900

    add: project1.js

---
+++ pageComponents/projects/project1.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import React from "react";
+
+const Root = styled.div`
+    
+`;
+
+const project1 = ({
+
+}) => {
+
+    return (
+        <Root>
+
+        </Root>
+    );
+}
+
+export default project1;
