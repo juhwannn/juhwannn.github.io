5dcc39a4a7116fc5f9aa15bcb86eccb1dab04c50
ahnjh <algorithm123@naver.com>
2022-04-26 21:07:13 +0900

    add: Info Component, Project Component

---
+++ pageComponents/Info.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import React from "react";
+
+const Root = styled.div`
+
+`;
+
+const Info =
+({
+
+}) => {
+
+    return (
+        <Root>
+            asdfsadf
+        </Root>
+    )
+};
+
+export default Info;
---
+++ pageComponents/Project.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Project =
+({
+
+}) => {
+
+    return (
+        <div>
+            asdf
+        </div>
+    );
+};
