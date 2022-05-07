f7817c1635b0a767435ea1022b714612a24d7a83
ahnjh <algorithm123@naver.com>
2022-05-07 17:48:29 +0900

    add: Header.js uppercase

--- pageComponents/elements/Header.js
+++ pageComponents/elements/Header.js
@@ -2,14 +2,18 @@ import styled from "styled-components";
 
 export const Header1 = styled.h1`
     font-size: 2rem;
+    
+    text-transform: uppercase;
 `;
 
 export const Header2 = styled.h2`
     color: #070707;
     
     font-size: 1.5rem;
+    text-transform: uppercase;
 `;
 
 export const Header3 = styled.h3`
     font-size: 1.17rem;
+    text-transform: uppercase;
 `;
