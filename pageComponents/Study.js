f665c19fdba0def3e60ff4207f2f5e3133a687f6
ahnjh <algorithm123@naver.com>
2022-05-10 17:33:52 +0900

    add: Study.js

---
+++ pageComponents/Study.js
@@ -0,0 +1,155 @@
+import styled from "styled-components";
+import React from "react";
+import {Description, Header1} from "./elements/Header";
+
+const Root = styled.div`
+
+`;
+
+const Study =
+({
+
+}) => {
+
+    return (
+        <Root>
+            <div>
+                <Header1>웹 서버와 서블릿 컨테이너 동작 원리</Header1>
+                <Description>
+
+                </Description>
+            </div>
+
+            <div>
+                <Header1>자바 빈</Header1>
+
+            </div>
+
+            <div>
+                <Header1>미들웨어</Header1>
+
+                <Description>
+                    미들웨어의 정의 : 미들웨어는 운영체제에서 제공하지 않는 일반적인 서비스와 기능을 애플리케이션에 제공하는 소프트웨어이다. <br/>
+                    데이터 관리, 애블리케이션 서비스, 메시징, 인증 및 API 관리는 주로 미들웨어를 통해 처리된다. <br/>
+                    미들웨어는 개발자들이 애플리케이션보다 효율적으로 구축할 수 있도록 지원하고 애플리케이션, 데이터 및 사용자 사이를 연결하는 요소처럼 작동한다.
+                </Description>
+            </div>
+
+            <div>
+                <Header1>결합의존도</Header1>
+            </div>
+
+            <div>
+                <Header1>서비스 의존도</Header1>
+            </div>
+
+            <div>
+                <Header1>Message queue</Header1>
+            </div>
+
+            <div>
+                <Header1>quartz</Header1>
+            </div>
+
+            <div>
+                <Header1>internal api</Header1>
+            </div>
+
+            <div>
+                <Header1>typescript</Header1>
+            </div>
+
+            <div>
+                <Header1>라이브러리와 api 차이</Header1>
+            </div>
+
+            <div>
+                <Header1>디자인 패턴</Header1>
+            </div>
+
+            <div>
+                <Header1>맥북에 jenkins 설치</Header1>
+            </div>
+
+            <div>
+                <Header1>express.js middleware 수행 규칙</Header1>
+            </div>
+
+            <div>
+                <Header1>express.js 가 Next.js 로 요청을 위임하는 방법</Header1>
+            </div>
+
+            <div>
+                <Header1>SPA</Header1>
+            </div>
+
+            <div>
+                <Header1>SSR</Header1>
+            </div>
+
+            <div>
+                <Header1>CSR</Header1>
+            </div>
+
+            <div>
+                <Header1>커밋 컨벤션</Header1>
+            </div>
+
+            <div>
+                <Header1>SAGA 패턴</Header1>
+            </div>
+
+            <div>
+                <Header1>SEO 최적화</Header1>
+            </div>
+
+            <div>
+                <Header1>document is not definded (ReactQuill 있는 페이지에서 리로딩시 에러)</Header1>
+            </div>
+
+            <div>
+                <Header1>DDD</Header1>
+            </div>
+
+            <div>
+                <Header1>Clean Architecture</Header1>
+            </div>
+
+            <div>
+                <Header1>Event Driven MicroService</Header1>
+            </div>
+
+            <div>
+                <Header1>LAMDA</Header1>
+            </div>
+
+            <div>
+                <Header1>React</Header1>
+            </div>
+
+            <div>
+                <Header1>Next.js</Header1>
+            </div>
+
+            <div>
+                <Header1>vercel.app</Header1>
+            </div>
+
+            <div>
+                <Header1>마크업 에디터</Header1>
+            </div>
+
+            <div>
+                <Header1>Next.js 라이프 사이클</Header1>
+            </div>
+
+            <div>
+                <Header1>면접준비 (기본개념)</Header1>
+            </div>
+
+            <div>
+                <Header1>제어 컴포넌트 / 비제어 컴포넌트</Header1>
+            </div>
+        </Root>
+    )
+}
