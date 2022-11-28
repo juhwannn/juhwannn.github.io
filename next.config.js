75ccb63b654a02b0801a2d9f505f0ce74caf1621
ahnjh <algorithm123@naver.com>
2022-11-28 20:24:44 +0900

    feat: webpack

--- next.config.js
+++ next.config.js
@@ -8,7 +8,10 @@ module.exports = {
   webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
-
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
     return config;
   },
 }
